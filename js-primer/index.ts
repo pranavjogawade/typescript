8482afb8a187fe17756c6d4b8c899952e01926b1
Pranav Jogawade <jogawadep@gmail.com>
2021-03-03 18:06:48 +0530

    typescript refrences added

--- js-primer/index.ts
+++ js-primer/index.ts
@@ -1,4 +1,18 @@
 let hatPrice = 100;
 console.log(`Hat Price: ${hatPrice}`);
+
 let bootPrice = 100;
 console.log(`Boot Price: ${bootPrice}`);
+
+if(hatPrice === bootPrice) {
+    console.log('Both same');
+}
+let totalPrice = hatPrice + bootPrice;
+console.log(`Total price is-: ${totalPrice}`);
+
+function sumPrices(test, ...number) {
+    console.log(number);
+    return number.reduce((total, val) => total+val, 0);
+}
+let total = sumPrices(1,2,3);
+console.log(total);
